0e1ab184b2ef8498c31714cf97588e9571cad71f
Nuku Malik Sanjaya Kusuma <nukumalik@gmail.com>
2019-11-20 16:23:48 +0700

    second merge

--- App.js
+++ App.js
@@ -6,16 +6,13 @@ import {createStackNavigator} from 'react-navigation-stack'
 import SplashScreen from './src/screens/SplashScreen'
 import Login from './src/screens/users/Login'
 import PreLogin from './src/screens/users/PreLogin'
-<<<<<<< HEAD
 import Home from './src/screens/Home'
 import Pin from './src/component/Pin'
 import ModalOTP from './src/component/ModalOTP'
 import ListBerita from './src/component/ListBerita'
-=======
 import ListTransaction from './src/screens/transactions/ListTransaction'
 import DetailTransaction from './src/screens/transactions/DetailTransaction'
 import Pulsa from './src/screens/pulses/Pulsa'
->>>>>>> 6902ddac931e2348aa8d02ebda9a3b426fc775af
 
 export default class App extends Component {
 	render() {
@@ -27,22 +24,16 @@ const StackNavigation = createStackNavigator(
 		SplashScreen: {screen: SplashScreen},
 		Login: {screen: Login},
 		PreLogin: {screen: PreLogin},
-<<<<<<< HEAD
 		Home: {screen: Home},
 		Pin: {screen: Pin},
 		ModalOTP: {screen: ModalOTP},
 		ListBerita: {screen: ListBerita},
-	},
-	{
-		initialRouteName: 'Home',
-=======
 		ListTransaction: {screen: ListTransaction},
 		DetailTransaction: {screen: DetailTransaction},
 		Pulsa: {screen: Pulsa},
 	},
 	{
-		initialRouteName: 'Pulsa',
->>>>>>> 6902ddac931e2348aa8d02ebda9a3b426fc775af
+		initialRouteName: 'Home',
 		headerMode: 'none',
 	}
 )
